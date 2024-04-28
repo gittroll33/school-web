1415c1a181a1c27a73b11e5b90e86d874e3a0355
David Klábeneš <david1klabenes@gmail.com>
2024-04-28 19:00:34 +0200

    console log

--- web html/script.js
+++ web html/script.js
@@ -1,5 +1,7 @@
 document.getElementById('signUpButton').addEventListener('click', function() {
     var link4 = document.getElementById('number4');
+    console.log('Sign-up button clicked');
     link4.classList.remove('disabled');
+    console.log('Link 4 should be enabled now');
     link4.classList.add('enabled');
 });
