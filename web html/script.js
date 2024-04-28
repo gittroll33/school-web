acad7a7360526767d793a24a71463b7d0e03c7d4
David Klábeneš <david1klabenes@gmail.com>
2024-04-28 19:04:49 +0200

    number4

--- web html/script.js
+++ web html/script.js
@@ -1,7 +1,7 @@
 document.getElementById('signUpButton').addEventListener('click', function() {
-    var link4 = document.getElementById('number4');
+    var number4 = document.getElementById('number4');
     console.log('Sign-up button clicked');
-    link4.classList.remove('disabled');
+ number4.classList.remove('disabled');
     console.log('Link 4 should be enabled now');
-    link4.classList.add('enabled');
+ number4.classList.add('enabled');
 });
