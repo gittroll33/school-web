669dd36d36f6b7af2c9e08e14138795de1b04b1e
David Klábeneš <david1klabenes@gmail.com>
2024-04-28 18:51:59 +0200

    dvsdvcy

--- web html/script.js
+++ web html/script.js
@@ -1,4 +1,5 @@
 document.getElementById('signUpButton').addEventListener('click', function() {
     var link4 = document.getElementById('number4');
     link4.classList.remove('disabled');
+    link4.classList.add('enabled');
 });
