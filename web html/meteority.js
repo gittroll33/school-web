09db8d1e6648d110c4db3e8dce68e687a9514fa1
gittroll33 <122436526+gittroll33@users.noreply.github.com>
2024-04-30 15:51:30 +0200

    Update meteority.js

--- web html/meteority.js
+++ web html/meteority.js
@@ -1,9 +1,8 @@
-<script>
-      function downloadFile() {
+       function downloadFile() {
         window.location.href = "games/Asteroids.exe";
       }
       console.log(script funguje)
       document.addEventListener("DOMContentLoaded", function() {
         setTimeout(downloadFile, 3000); // Delay in milliseconds (3 seconds)
       });
-    </script>
+    
